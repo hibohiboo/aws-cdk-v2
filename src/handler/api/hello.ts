5576de6e9545842f3b22fd2675f8216921cf102b
hibohiboo <swdice66@gmail.com>
2021-12-05 22:14:34 +0900

    時間を追加

--- src/handler/api/hello.ts
+++ src/handler/api/hello.ts
@@ -1,11 +1,12 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { formatDate } from '../../common/utils/date-util';
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-
+  const datetime = formatDate(new Date(event.requestContext.requestTimeEpoch))
   return {
     'statusCode': 200,
     'body': JSON.stringify({
-      message: 'hello world',
+      message: `hello world. ${datetime} = ${event.requestContext.requestTimeEpoch}`,
     })
   };
 };
