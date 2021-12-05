5f67fee5454abffda4ea1336ce421d319f7304a3
hibohiboo <swdice66@gmail.com>
2021-12-05 22:27:58 +0900

    bundle

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -3,15 +3,27 @@ import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from 'path'
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    const helloLambda = new NodejsFunction(this, 'test', {
+    const nodeModulesLayer = new lambda.LayerVersion(this, 'NodeModulesLayer',
+      {
+        code: lambda.AssetCode.fromAsset(path.resolve(process.cwd(), './bundle')),
+        compatibleRuntimes: [lambda.Runtime.NODEJS_14_X]
+      }
+    );
+    const helloLambda = new NodejsFunction(this, 'helloLambda', {
       runtime: lambda.Runtime.NODEJS_14_X,
       entry: `../src/handler/api/hello.ts`,
+      layers: [nodeModulesLayer],
+      bundling: {
+        externalModules: [
+          'aws-sdk', // Use the 'aws-sdk' available in the Lambda runtime
+          'date-fns', // Layrerに入れておきたいモジュール
+        ],
+      }
     });
     const api = new RestApi(this, 'ServerlessRestApi', { cloudWatchRole: false });
     api.root.addResource('hello').addMethod('GET', new LambdaIntegration(helloLambda));
