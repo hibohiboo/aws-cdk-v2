cb9cebf10f0a7923a89563def32e9ad1141069f5
hibohiboo <swdice66@gmail.com>
2021-12-30 13:41:01 +0900

    add docker sample #3

---
+++ docker/postgres/docker-entrypoint-initdb.d/03-insert_table.sql
@@ -0,0 +1,4 @@
+
+insert into electric(id,name,measuredtime,value) values(1,'test1',now(),100);
+insert into electric(id,name,measuredtime,value) values(2,'test2',now(),10.11);
+insert into electric(id,name,measuredtime,value) values(3,'test3',now(),300);
---
+++ src/handler/invoke/electric/read.ts
@@ -0,0 +1,12 @@
+import type { Handler } from 'aws-lambda';
+import { format } from 'date-fns';
+import { formatDate } from '@/common/index';
+import { getPostgresClient } from '@/common/persistants/portgres';
+
+export const handler: Handler = async () => {
+  const client = await getPostgresClient();
+  const result = await client.execute('select * from electric');
+  await client.release();
+  console.log(result);
+  return JSON.stringify(result);
+};
