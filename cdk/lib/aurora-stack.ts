0f3e1b37d45ab44e65ec7b2a01b4818a2f3e60e3
hibohiboo <swdice66@gmail.com>
2022-01-10 00:16:56 +0900

    fix リード/レプリカ 構成 #4

--- cdk/lib/aurora-stack.ts
+++ cdk/lib/aurora-stack.ts
@@ -1,7 +1,7 @@
 import { Aspects, RemovalPolicy, Stack, StackProps, Tag, Tags } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { InstanceClass, InstanceSize, InstanceType, SecurityGroup, Vpc } from 'aws-cdk-lib/aws-ec2';
-import { AuroraPostgresEngineVersion, Credentials, DatabaseCluster, DatabaseClusterEngine, ParameterGroup, SubnetGroup } from 'aws-cdk-lib/aws-rds';
+import { AuroraPostgresEngineVersion, Credentials, DatabaseCluster, DatabaseClusterEngine, DatabaseSecret, ParameterGroup, SubnetGroup } from 'aws-cdk-lib/aws-rds';
 import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import { AccountPrincipal, Role } from 'aws-cdk-lib/aws-iam';
 
@@ -27,15 +27,15 @@ export class AuroraStack extends Stack {
 
     const vpc = Vpc.fromLookup(this, 'Vpc', { vpcId: props.vpcId })
     const securityGroup = SecurityGroup.fromLookupById(this, 'SecurityGroup', props.sgId);
-    // subnetGroupNameはlowecaseで作成される
+
+    // subnetGroupNameはlowecaseで作成されている
     const subnetGroup = SubnetGroup.fromSubnetGroupName(this, 'SubnetGroup', props.subnetGroupName.toLowerCase());
 
-    const secret = this.createSecret({ secretName: props.dbAdminSecretName, rdsName: props.dbAdminName });
-
-    // default..aurora-postgresql11が見つからない。。
-    // const AURORA_POSTGRES_ENGINE_VERSION = AuroraPostgresEngineVersion.VER_11_9; // LTSのバージョンを選択 2021.12.10
-    // const RDS_MAJOR_VERSION = AURORA_POSTGRES_ENGINE_VERSION.auroraPostgresMajorVersion.split('.')[0]
-    // const parameterGroup = ParameterGroup.fromParameterGroupName(this, 'DBParameterGroup', `default.aurora-postgresql${RDS_MAJOR_VERSION}`)
+    const secret = new DatabaseSecret(this, props.dbAdminSecretName, {
+      secretName: props.dbAdminSecretName,
+      username: props.dbAdminName
+    });
+    Tags.of(secret).add('Name', props.dbAdminSecretName);
 
     const cluster = new DatabaseCluster(this, 'clusterForAurora', {
       // LTSのバージョンを選択.RDSProxyは10と11のみのサポート 2021.12.10
@@ -48,23 +48,29 @@ export class AuroraStack extends Stack {
         // postgresを使える最安値 (2021.12.10)
         instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.MEDIUM),
       },
-      instances: 1,
+      instances: 2,
       subnetGroup,
       // https://dev.classmethod.jp/articles/cdk-practice-21-rds-parameter-group/
       parameterGroup: new ParameterGroup(this, 'rds params', {
         engine: DatabaseClusterEngine.auroraPostgres({ version: AuroraPostgresEngineVersion.VER_11_9 }),
         description: 'Cluster Parameter Group for RDS',
-        parameters: { time_zone: 'JST' },
+        parameters: {
+          log_rotation_age: '30' // デフォルト60分。
+        },
       }),
       credentials: Credentials.fromSecret(secret)
     });
 
     // https://docs.aws.amazon.com/cdk/api/v2//docs/aws-cdk-lib.aws_rds-readme.html
     // パスワードのローテーション設定。 ローテーション感覚はデフォルトは30日。パスワードに使用しない文字はデフォルトで、" %+~`#$&*()|[]{}:;<>?!'/@\"\\"
-    cluster.addRotationSingleUser();
+    // cluster.addRotationSingleUser();
 
     // RDSでの作成ユーザをシークレットに登録
-    const secretForDBUser = this.createSecret({ secretName: props.dbReadOnlyUserSecretName, rdsName: props.dbReadOnlyUserName });
+    const secretForDBUser = new DatabaseSecret(this, props.dbReadOnlyUserSecretName, {
+      secretName: props.dbReadOnlyUserSecretName,
+      username: props.dbReadOnlyUserName
+    });
+    Tags.of(secret).add('Name', props.dbAdminSecretName);
 
     const proxy = cluster.addProxy('Proxy', {
       secrets: [cluster.secret!, secretForDBUser],
@@ -77,24 +83,10 @@ export class AuroraStack extends Stack {
 
     const role = new Role(this, 'DBProxyRole', { assumedBy: new AccountPrincipal(this.account) });
     Tags.of(role).add('Name', 'AuroraProxyRole');
-    proxy.grantConnect(role, props.dbAdminName); // Grant the role connection access to the DB Proxy for database user 'admin'.
+    proxy.grantConnect(role, props.dbAdminName);
     proxy.grantConnect(role, props.dbReadOnlyUserName);
 
     // 作成したリソース全てにタグをつける
     Aspects.of(this).add(new Tag('Stack', id));
-  }
-
-  private createSecret(props: { secretName: string, rdsName: string }) {
-    const secret = new Secret(this, props.secretName, {
-      secretName: props.secretName,
-      generateSecretString: {
-        secretStringTemplate: JSON.stringify({ username: props.rdsName }),
-        excludePunctuation: true, // '、/、"、@、スペースはpostgresのパスワードに利用できないので除外
-        includeSpace: false,
-        generateStringKey: 'password'
-      }
-    })
-    Tags.of(secret).add('Name', props.secretName);
-    return secret;
   }
 }
