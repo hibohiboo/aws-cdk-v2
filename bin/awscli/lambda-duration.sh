87fd6a75b0aad8122efe9ec4b9a86ab61df137e0
hibohiboo <swdice66@gmail.com>
2022-03-18 08:22:27 +0900

    add tmp awscli

---
+++ bin/awscli/lambda-duration.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 環境変数読込
+. ./.env
+
+# このシェルスクリプトのディレクトリの絶対パスを取得。
+bin_dir=$(cd $(dirname $0) && pwd)
+
+# 変数の設定
+
+# メトリクスの取得
+tmp_result=`aws cloudwatch get-metric-data \
+--metric-data-queries $(cat query.json | jq -c) \
+--start-time 2022-03-12T04:00:00Z \
+--end-time 2022-03-12T10:00:00Z 
+`
+
+mkdir -p $bin_dir/result
+
+echo $tmp_result > $bin_dir/result/result.log
