ce8ee6bdbcb23a0921ffa370c9be0485fb3beb16
hibohiboo <swdice66@gmail.com>
2023-03-05 21:33:22 +0900

    fix 気温取得

--- docker-python/src/teamperature.py
+++ docker-python/src/teamperature.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 time_diff = timedelta(hours=9)
 
-gpv_file = pygrib.open("Z__C_RJTD_20171205000000_MSM_GPV_Rjp_Lsurf_FH00-15_grib2.bin")
+gpv_file = pygrib.open("/grib2/Z__C_RJTD_20171205000000_MSM_GPV_Rjp_Lsurf_FH00-15_grib2.bin")
 t_messages = gpv_file.select(name="Temperature")
 
 df = pd.DataFrame({
