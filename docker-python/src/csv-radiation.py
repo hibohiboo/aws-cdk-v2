024beb31cc54820afbeea0afd065f0b98459300b
hibohiboo <swdice66@gmail.com>
2023-03-05 22:42:46 +0900

    add grib2csv

---
+++ docker-python/src/csv-radiation.py
@@ -0,0 +1,30 @@
+from datetime import timedelta
+import pygrib
+import pandas as pd
+
+time_diff = timedelta(hours=9)
+
+gpv_file = pygrib.open("/grib2/Z__C_RJTD_20171205000000_MSM_GPV_Rjp_Lsurf_FH00-15_grib2.bin")
+t_messages = gpv_file.select(name="Downward short-wave radiation flux")
+
+# 日射量データ取得用配列の初期化
+radiation_data = []
+
+# データの探索
+for grb in t_messages:
+    values, lats, lons = grb.data()
+    jst =  grb.validDate + time_diff
+
+    for i, lat in enumerate(lats):
+        for j, x in enumerate(lat):
+          radiation_data.append([jst, lats[i][j], lons[i][j], values[i][j]])
+    
+
+# ファイルクローズ
+gpv_file.close()
+
+# DataFrameの作成
+df = pd.DataFrame(radiation_data, columns=['Date', 'Latitude', 'Longitude', 'Radiation'])
+
+
+print(df)
---
+++ docker-python/src/radiation.py
@@ -0,0 +1,22 @@
+from datetime import timedelta
+import pygrib
+import pandas as pd
+
+time_diff = timedelta(hours=9)
+
+gpv_file = pygrib.open("/grib2/Z__C_RJTD_20171205000000_MSM_GPV_Rjp_Lsurf_FH00-15_grib2.bin")
+t_messages = gpv_file.select(name="Downward short-wave radiation flux")
+
+df = pd.DataFrame({
+    "validDate": [msg.validDate + time_diff for msg in t_messages],
+    "radiation": [
+        msg.data(
+            lat1=35.6745-0.025,
+            lat2=35.6745+0.025,
+            lon1=139.7169-0.03125,
+            lon2=139.7169+0.03125,
+        )[0][0][0] for msg in t_messages
+    ]
+})
+
+print(df)
