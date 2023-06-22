03297ecc7c724782aa33d3378d7967e7af81c8b4
hibohiboo <swdice66@gmail.com>
2023-06-23 00:20:46 +0900

    fix esbuild

--- cdk/constants/lambda-layer.ts
+++ cdk/constants/lambda-layer.ts
@@ -1,7 +1,10 @@
 
 export const externalModules = [
-  'aws-sdk', // Use the 'aws-sdk' available in the Lambda runtime
+  'aws-sdk', // Use the 'aws-sdk' available in the Lambda runtime ... v2
   'date-fns', // Layrerに入れておきたいモジュール
   'pg',
-  'aws-jwt-verify'
+  'aws-jwt-verify',
+  // ↓ sdk v3
+  '@aws-sdk/client-cloudwatch',
+  '@aws-sdk/client-sns'
 ]
