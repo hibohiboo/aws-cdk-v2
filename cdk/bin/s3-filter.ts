aa55781c55cbac602476b70f1505c240b2181a05
hibohiboo <hibohiboo66+github@gmail.com>
2023-10-24 08:50:47 +0900

    add s3filter sample

---
+++ cdk/bin/s3-filter.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import * as dotenv from 'dotenv';
+import { S3FilterStack } from '../lib/s3-filter-stack';
+
+dotenv.config();
+
+const envList = ['PROJECT_ID'] as const;
+for (const key of envList) {
+  if (!process.env[key]) throw new Error(`please add ${key} to .env`);
+}
+const processEnv = process.env as Record<(typeof envList)[number], string>;
+
+const app = new cdk.App();
+
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+const stackId = `${processEnv.PROJECT_ID}-s3-filter`;
+new S3FilterStack(app, stackId, {
+  bucketName: `${stackId}-bucket`,
+  topicName: `${stackId}-topic`,
+  projectNameTag: stackId,
+  env,
+});
--- cdk/lib/s3-filter-stack.ts
+++ cdk/lib/s3-filter-stack.ts
@@ -1,8 +1,100 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, RemovalPolicy, Stack, StackProps, Tags } from 'aws-cdk-lib';
+import {
+  AnyPrincipal,
+  Effect,
+  PolicyDocument,
+  PolicyStatement,
+} from 'aws-cdk-lib/aws-iam';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import {
+  BlockPublicAccess,
+  Bucket,
+  BucketAccessControl,
+  EventType,
+} from 'aws-cdk-lib/aws-s3';
+import s3Notify from 'aws-cdk-lib/aws-s3-notifications';
+import { Topic, TopicPolicy } from 'aws-cdk-lib/aws-sns';
+import { LambdaSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
 import { Construct } from 'constructs';
-
+interface S3FilterStackProps extends StackProps {
+  projectNameTag: string;
+  bucketName: string;
+  topicName: string;
+}
 export class S3FilterStack extends Stack {
-  constructor(scope: Construct, id: string, props: StackProps) {
+  constructor(scope: Construct, id: string, props: S3FilterStackProps) {
     super(scope, id, props);
+    const bucket = this.createBucket(props);
+    this.createSNS(props, bucket);
+    Tags.of(this).add('Project', props.projectNameTag);
+  }
+  private createBucket(props: { bucketName: string }) {
+    const bucket = new Bucket(this, props.bucketName, {
+      bucketName: props.bucketName,
+      accessControl: BucketAccessControl.PRIVATE,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      lifecycleRules: [{ expiration: Duration.days(90) }],
+    });
+    return bucket;
+  }
+  private createSNS(props: { topicName: string }, bucket: Bucket) {
+    const sns = new Topic(this, props.topicName, {
+      displayName: props.topicName,
+      topicName: props.topicName,
+      fifo: false,
+    });
+    new TopicPolicy(this, `${props.topicName}-policy`, {
+      topics: [sns],
+      policyDocument: new PolicyDocument({
+        statements: [
+          new PolicyStatement({
+            actions: [
+              'SNS:GetTopicAttributes',
+              'SNS:SetTopicAttributes',
+              'SNS:AddPermission',
+              'SNS:RemovePermission',
+              'SNS:DeleteTopic',
+              'SNS:Subscribe',
+              'SNS:ListSubscriptionsByTopic',
+              'SNS:Publish',
+              'SNS:Receive',
+            ],
+            effect: Effect.ALLOW,
+            resources: [sns.topicArn],
+            principals: [new AnyPrincipal()],
+          }),
+        ],
+      }),
+    });
+    bucket.addEventNotification(
+      EventType.OBJECT_CREATED_COPY,
+      new s3Notify.SnsDestination(sns),
+      { prefix: 'ptest/' },
+    );
+    return sns;
+  }
+  private createLambda(sns: Topic) {
+    const lambda = new NodejsFunction(this, 'describeLambda', {
+      runtime: Runtime.NODEJS_18_X,
+      entry: `../src/handler/invoke/describeSNS.ts`,
+    });
+    // https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_sns_subscriptions.LambdaSubscription.html
+    sns.addSubscription(
+      new LambdaSubscription(lambda, {
+        filterPolicyWithMessageBody: {
+          // Lambda should receive only message matching the following conditions on message body:
+          // color: 'red' or 'orange'
+          // background: sns.FilterOrPolicy.policy({
+          //   color: sns.FilterOrPolicy.filter(sns.SubscriptionFilter.stringFilter({
+          //     allowlist: ['red', 'orange'],
+          //   })),
+          // }),
+        },
+      }),
+    );
+    return lambda;
   }
 }
