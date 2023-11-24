2146b37f418783c3d8ba4e7589bbe5e8bb44c905
hibohiboo <hibohiboo66+github@gmail.com>
2023-11-24 18:42:15 +0900

    fix name to paramterName

--- docker-python/src/get-rad-tem.py
+++ docker-python/src/get-rad-tem.py
@@ -32,7 +32,7 @@ lon = json_obj["lon"]
 
 # https://predora005.hatenablog.com/entry/2020/10/31/000000
 gpv_file = pygrib.open("/grib2/Z__C_RJTD_20171205000000_MSM_GPV_Rjp_Lsurf_FH00-15_grib2.bin")
-t_messages = gpv_file.select(name="Downward short-wave radiation flux")
+t_messages = gpv_file.select(parameterName="Downward short-wave radiation flux")
 
 # 日射量データ取得用配列の初期化
 radiation_data = []
