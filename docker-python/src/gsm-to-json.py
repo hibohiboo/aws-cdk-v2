d76c8213ee19581bab229dff71daecfd61ef2b69
hibohiboo <hibohiboo66+github@gmail.com>
2023-11-24 22:31:21 +0900

    fix map

--- docker-python/src/gsm-to-json.py
+++ docker-python/src/gsm-to-json.py
@@ -32,18 +32,30 @@ t_messages = gpv_file.select(parameterName="Downward short-wave radiation flux")
 
 # 日射量データ取得用配列の初期化
 radiation_data = []
-
+analDate = ""
+validDates = []
+dataMap = {}
 # データの探索
 for grb in t_messages:
     values, lats, lons = grb.data(lat1=la1,lat2=la2,lon1=lo1,lon2=lo2)
     jst =  grb.validDate + time_diff
     jst2 =  grb.analDate + time_diff
+    validDates.append(util.t2s(jst))
+    analDate = util.t2s(jst2)
     for i, lat in enumerate(lats):
         for j, x in enumerate(lat):
-          radiation_data.append([{"validDate": util.t2s(jst),"analDate": util.t2s(jst2), "lat": util.round_up_to_5_digits(lats[i][j]), "lon": util.round_up_to_5_digits(lons[i][j]), "radiation": util.round_up_to_5_digits(values[i][j])}])
+          la = util.round_up_to_5_digits(lats[i][j])
+          lo = util.round_up_to_5_digits(lons[i][j])
+          key = str(la) + "_" + str(lo)
+          if key not in dataMap:
+            dataMap[key] = {}
+          dataMap[key][util.t2s(jst)] = util.round_up_to_5_digits(values[i][j])
 
-print(radiation_data)
+# print(radiation_data)
 
 with open("/dist/output.json", 'w') as file:
     # ファイルに書き込む
-    file.write(json.dumps(radiation_data))
+    file.write(json.dumps(dataMap))
+with open("/dist/dates.json", 'w') as file:
+    # ファイルに書き込む
+    file.write(json.dumps(validDates))
