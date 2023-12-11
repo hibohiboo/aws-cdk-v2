afce3233379354ced79dc9c728d232febd71d5ec
hibohiboo <hibohiboo66+github@gmail.com>
2023-12-11 21:10:39 +0900

    fix 日付算出方法の変更

--- docker-python/src/gsm-to-json.py
+++ docker-python/src/gsm-to-json.py
@@ -20,21 +20,22 @@ lon = json_obj["lon"]
 # FILE_NAME = "Z__C_RJTD_20231124000000_GSM_GPV_Rjp_Gll0p1deg_Lsurf_FD0000-0100_grib2"
 # FILE_NAME = "Z__C_RJTD_20231124000000_GSM_GPV_Rjp_Gll0p1deg_Lsurf_FD0101-0200_grib2"
 # FILE_NAME = "Z__C_RJTD_20231124000000_GSM_GPV_Rjp_Gll0p1deg_Lsurf_FD0201-0300_grib2"
-FILE_NAME = "Z__C_RJTD_20231124000000_GSM_GPV_Rjp_Gll0p1deg_Lsurf_FD0401-0500_grib2"
+# FILE_NAME = "Z__C_RJTD_20231124000000_GSM_GPV_Rjp_Gll0p1deg_Lsurf_FD0401-0500_grib2"
 # FILE_NAME = "Z__C_RJTD_20231124000000_GSM_GPV_Rjp_Gll0p1deg_Lsurf_FD0501-0512_grib2"
+FILE_NAME = "Z__C_RJTD_20231124000000_GSM_GPV_Rjp_Gll0p1deg_Lsurf_FD0515-0700_grib2"
 # http://database.rish.kyoto-u.ac.jp/arch/jmadata/data/gpv/original/2023/11/24/ より
 gpv_file = pygrib.open("/grib2/" + FILE_NAME + ".bin")
 
 analDate = grib.getBaseData(gpv_file, lat, lon,  LAT_STEP, LON_STEP)
 
 temperature = grib.getParamData(gpv_file, "Temperature", lat, lon, LAT_STEP, LON_STEP)
-radiation = grib.getParamDataMaybeFirstNone(gpv_file, "Downward short-wave radiation flux", lat, lon, LAT_STEP, LON_STEP)
+radiation = grib.getParamData(gpv_file, "Downward short-wave radiation flux", lat, lon, LAT_STEP, LON_STEP)
 pressure = grib.getParamData(gpv_file, "Pressure", lat, lon, LAT_STEP, LON_STEP)
 mslp = grib.getParamData(gpv_file, "Pressure reduced to MSL", lat, lon, LAT_STEP, LON_STEP)
 uwind = grib.getParamData(gpv_file, "u-component of wind", lat, lon, LAT_STEP, LON_STEP)
 vwind = grib.getParamData(gpv_file, "v-component of wind", lat, lon, LAT_STEP, LON_STEP)
 rh = grib.getParamData(gpv_file, "Relative humidity", lat, lon, LAT_STEP, LON_STEP)
-rain = grib.getParamDataMaybeFirstNone(gpv_file, "Total precipitation", lat, lon, LAT_STEP, LON_STEP)
+rain = grib.getParamData(gpv_file, "Total precipitation", lat, lon, LAT_STEP, LON_STEP)
 lcloud = grib.getParamData(gpv_file, "Low cloud cover", lat, lon, LAT_STEP, LON_STEP)
 mcloud = grib.getParamData(gpv_file, "Medium cloud cover", lat, lon, LAT_STEP, LON_STEP)
 hcloud = grib.getParamData(gpv_file, "High cloud cover", lat, lon, LAT_STEP, LON_STEP)
--- docker-python/src/lib/grib.py
+++ docker-python/src/lib/grib.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import timedelta, datetime
 from lib import util
 
 # 日本時間に直す用
@@ -17,34 +17,9 @@ def getParamData(gpv_file, parameterName, lat, lon, LAT_STEP, LON_STEP):
   for grb in t_messages:
       values, lats, lons = grb.data(lat1=la1,lat2=la2,lon1=lo1,lon2=lo2)
       # 予報時刻(UTC)を日本時間に直す
-      jst =  grb.validDate + time_diff
-      for i, lat in enumerate(lats):
-          for j, x in enumerate(lat):
-            la = util.round_up_to_5_digits(lats[i][j])
-            lo = util.round_up_to_5_digits(lons[i][j])
-            key = str(la) + "_" + str(lo)
-            if key not in dataMap:
-              dataMap[key] = {}
-            dataMap[key][util.t2s(jst)] = util.round_up_to_5_digits(values[i][j])
-
-  return dataMap
-
-# 日射量と降水量は１時間間ずれる
-def getParamDataMaybeFirstNone(gpv_file, parameterName, lat, lon, LAT_STEP, LON_STEP):
-  la1 = lat - LAT_STEP
-  la2 = lat + LAT_STEP
-  lo1 = lon - LON_STEP
-  lo2 = lon + LON_STEP
-
-  t_messages = gpv_file.select(parameterName=parameterName)
-
-  bias = 1
-  dataMap = {}
-  # データの探索
-  for grb in t_messages:
-      values, lats, lons = grb.data(lat1=la1,lat2=la2,lon1=lo1,lon2=lo2)
-
-      jst =  grb.validDate + time_diff + timedelta(hours=bias)
+      # jst =  grb.validDate + time_diff
+      # validDateは「Total precipitation」と「Downward short-wave radiation flux」では不適切なので、validtyDateとvalidityTimeを使う
+      jst =  datetime.strptime(str(grb.validityDate) + str(grb.validityTime).zfill(4), "%Y%m%d%H%M") + time_diff
       for i, lat in enumerate(lats):
           for j, x in enumerate(lat):
             la = util.round_up_to_5_digits(lats[i][j])
