9df19a2f5406980b112649375399f6289bd33288
hibo <hibohiboo66@gmail.com>
2024-02-01 21:21:46 +0900

    fix def body

--- cdk/bin/step-functions.ts
+++ cdk/bin/step-functions.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { config } from 'dotenv'
 import { StepFunctionSampleStack } from '../lib/step-functions-stack';
+
 config();
 
 const app = new cdk.App();
@@ -11,25 +12,7 @@ const env = {
   region: process.env.CDK_DEFAULT_REGION
 }
 
-const envNames = ['VPC_ID', 'PRIVATE_LAMBDA_SG_ID', 'DB_PROXY_RESOURCE_ID', 'DB_ADMIN_NAME', 'DB_PROXY_ENDPOINT', 'DB_USER_NAME', 'DB_PROXY_READ_ONLY_ENDPOINT'] as const
-const checkEnvs = (e: any): e is Record<(typeof envNames)[number], string> => {
-  for (const a of envNames) {
-    console.log(a, e[a]);
-    if (!e[a]) throw new Error(`please set environment variable ${a}`)
-  }
-  return true
-}
-if (!checkEnvs(process.env)) throw new Error('到達しない')
 
-new StepFunctionSampleStack(app, 'StepFunctionSampleStack', {
-  env,
-  vpcId: process.env.VPC_ID,
-  sgId: process.env.PRIVATE_LAMBDA_SG_ID,
-  rdsProxyResourceId: process.env.DB_PROXY_RESOURCE_ID,
-  dbAdminName: process.env.DB_ADMIN_NAME,
-  dbProxyEndpoint: process.env.DB_PROXY_ENDPOINT,
-  dbReadOnlyUserName: process.env.DB_USER_NAME,
-  dbProxyReadOnlyEndpoint: process.env.DB_PROXY_READ_ONLY_ENDPOINT
-});
+new StepFunctionSampleStack(app, 'StepFunctionSampleStack', { env });
 
 
--- cdk/lib/step-functions-stack.ts
+++ cdk/lib/step-functions-stack.ts
@@ -2,31 +2,16 @@ import * as cdk from 'aws-cdk-lib';
 import * as stepfunc from 'aws-cdk-lib/aws-stepfunctions';
 import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import { SecurityGroup, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { aws_events, aws_events_targets } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { DefinitionBody } from 'aws-cdk-lib/aws-stepfunctions';
 
-interface StepFunctionSampleStackProps extends cdk.StackProps {
-  vpcId: string
-  sgId: string
-  rdsProxyResourceId: string
-  dbAdminName: string
-  dbProxyEndpoint: string
-  dbReadOnlyUserName: string
-  dbProxyReadOnlyEndpoint: string
-}
+interface StepFunctionSampleStackProps extends cdk.StackProps { }
 export class StepFunctionSampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: StepFunctionSampleStackProps) {
     super(scope, id, props);
-
-    const vpc = Vpc.fromLookup(this, 'Vpc', { vpcId: props.vpcId })
-    const securityGroup = SecurityGroup.fromLookupById(this, 'SecurityGroup', props.sgId);
-    const vpcSubnets = vpc.selectSubnets({ subnetType: SubnetType.PRIVATE_ISOLATED })
     const lambdaParamsDefault = {
-      vpc,
-      vpcSubnets,
-      securityGroups: [securityGroup],
-      runtime: lambda.Runtime.NODEJS_18_X,
+      runtime: lambda.Runtime.NODEJS_20_X,
       handler: 'index.handler',
       timeout: cdk.Duration.seconds(10),
       // layers: [nodeModulesLayer],
@@ -67,14 +52,14 @@ export class StepFunctionSampleStack extends cdk.Stack {
     // StateMachine
     const definition = upstreamJob.next(mainJob).next(new stepfunc.Succeed(this, 'Queued'));
     const stateMachine = new stepfunc.StateMachine(this, 'StepFunctionSampleStateMachine', {
-      definition,
+      definitionBody: DefinitionBody.fromChainable(definition),
     });
 
     // EventBridge Rule1（毎時15〜30分に起動）
     new aws_events.Rule(this, 'rule1', {
       ruleName: 'sampleRule1',
       schedule: aws_events.Schedule.cron({
-        minute: '40-50',
+        minute: '15-30',
       }),
       targets: [new aws_events_targets.SfnStateMachine(stateMachine)],
     });
