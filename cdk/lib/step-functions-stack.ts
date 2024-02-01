5b6638208cbe8cf90487be051bbbdb7c5a27580e
hibo <hibohiboo66@gmail.com>
2024-02-02 07:51:31 +0900

    fix step

--- cdk/lib/step-functions-stack.ts
+++ cdk/lib/step-functions-stack.ts
@@ -4,11 +4,21 @@ import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { aws_events, aws_events_targets } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Choice, Condition, DefinitionBody, Pass, Result } from 'aws-cdk-lib/aws-stepfunctions';
+import {
+  Choice,
+  Condition,
+  DefinitionBody,
+  Pass,
+  Result,
+} from 'aws-cdk-lib/aws-stepfunctions';
 
-interface StepFunctionSampleStackProps extends cdk.StackProps { }
+interface StepFunctionSampleStackProps extends cdk.StackProps {}
 export class StepFunctionSampleStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props: StepFunctionSampleStackProps) {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: StepFunctionSampleStackProps,
+  ) {
     super(scope, id, props);
     const lambdaParamsDefault = {
       runtime: lambda.Runtime.NODEJS_20_X,
@@ -16,7 +26,7 @@ export class StepFunctionSampleStack extends cdk.Stack {
       timeout: cdk.Duration.seconds(10),
       // layers: [nodeModulesLayer],
       // bundling
-    }
+    };
 
     // Lambda samples
     const firstFunction = new lambda.Function(this, 'FirstFunction', {
@@ -24,8 +34,7 @@ export class StepFunctionSampleStack extends cdk.Stack {
       code: lambda.Code.fromInline(`
         exports.handler = async (event) => {
           console.log(event);
-          const Payload = { message: "hello", firstEvent: event }
-          return { Payload }
+          return 'first';
         };
       `),
     });
@@ -34,7 +43,7 @@ export class StepFunctionSampleStack extends cdk.Stack {
       code: lambda.Code.fromInline(`
       exports.handler = async (event) => {
         console.log(event);
-        return event.firstEvent;
+
       };
       `),
     });
@@ -59,12 +68,13 @@ exports.handler = async (event) => {
     // definite state machine
     const firstJonb = new tasks.LambdaInvoke(this, 'UpstreamTask', {
       lambdaFunction: firstFunction,
-      outputPath: '$.Payload',
+      resultPath: '$.hoge',
+      payloadResponseOnly: true,
     });
     const secondJob = new tasks.LambdaInvoke(this, 'MainFunctionTask', {
       lambdaFunction: secondFunction,
-      payload: stepfunc.TaskInput.fromJsonPathAt('$.Payload'),
-      outputPath: '$.Payload',
+      resultPath: '$.huga',
+      payloadResponseOnly: true,
     });
 
     const configureCount = new Pass(this, 'ConfigureCount', {
@@ -74,24 +84,36 @@ exports.handler = async (event) => {
     const iteratorJob = new tasks.LambdaInvoke(this, 'Iterator', {
       lambdaFunction: iteratorFunction,
       payloadResponseOnly: true,
-      retryOnServiceExceptions: false
+      retryOnServiceExceptions: false,
     });
     const exampleWork = new Pass(this, 'ExampleWork', {
-      comment: "Your application logic, to run a specific number of times",
+      comment: 'Your application logic, to run a specific number of times',
       result: Result.fromObject({ success: true }),
       resultPath: '$.result',
     });
     const isCountReached = new Choice(this, 'IsCountReached');
     const condition1 = Condition.booleanEquals('$.iterator.continue', true);
     // StateMachine
-    const iterators = configureCount.next(iteratorJob).next(
-      isCountReached.when(condition1, exampleWork.next(iteratorJob)).otherwise(new Pass(this, 'Done'))
+    const loopJob = exampleWork
+      .next(firstJonb)
+      .next(secondJob)
+      .next(iteratorJob);
+    const iterators = configureCount
+      .next(iteratorJob)
+      .next(
+        isCountReached
+          .when(condition1, loopJob)
+          .otherwise(new Pass(this, 'Done')),
+      );
+    const definition = iterators;
+    const stateMachine = new stepfunc.StateMachine(
+      this,
+      'StepFunctionSampleStateMachine',
+      {
+        comment: 'Iterator State Machine Example',
+        definitionBody: DefinitionBody.fromChainable(definition),
+      },
     );
-    const definition = firstJonb.next(secondJob).next(iterators);
-    const stateMachine = new stepfunc.StateMachine(this, 'StepFunctionSampleStateMachine', {
-      comment: "Iterator State Machine Example",
-      definitionBody: DefinitionBody.fromChainable(definition),
-    });
 
     // EventBridge Rule1（毎時5分に起動）
     new aws_events.Rule(this, 'rule1', {
