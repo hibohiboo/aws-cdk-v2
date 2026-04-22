d756f989bc9590325a9798e66545c0d008a8b7fa
hibohiboo <hibohiboo66+github@gmail.com>
2026-04-22 23:56:32 +0900

    add rdb

--- cdk-2026/lib/index.ts
+++ cdk-2026/lib/index.ts
@@ -1,21 +1,64 @@
-// import * as cdk from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
 
 export interface Cdk2026Props {
-  // Define construct properties here
+  vpc?: ec2.IVpc;
 }
 
 export class Cdk2026 extends Construct {
+  public readonly vpc: ec2.IVpc;
+  public readonly auroraCluster: rds.DatabaseCluster;
+  public readonly auroraSecurityGroup: ec2.SecurityGroup;
 
   constructor(scope: Construct, id: string, props: Cdk2026Props = {}) {
     super(scope, id);
 
-    // Define construct contents here
+    this.vpc = props.vpc ?? new ec2.Vpc(this, 'Vpc', {
+      maxAzs: 2,
+      natGateways: 1,
+      subnetConfiguration: [
+        {
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+        {
+          name: 'Private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 24,
+        },
+        {
+          name: 'Isolated',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Cdk2026Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    this.auroraSecurityGroup = new ec2.SecurityGroup(this, 'AuroraSecurityGroup', {
+      vpc: this.vpc,
+      description: 'Security group for Aurora MySQL Serverless V2',
+      allowAllOutbound: false,
+    });
+
+    this.auroraCluster = new rds.DatabaseCluster(this, 'AuroraCluster', {
+      engine: rds.DatabaseClusterEngine.auroraMysql({
+        version: rds.AuroraMysqlEngineVersion.VER_3_12_0,
+      }),
+      writer: rds.ClusterInstance.serverlessV2('writer'),
+      serverlessV2MinCapacity: 0.5,
+      serverlessV2MaxCapacity: 4,
+      vpc: this.vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+      },
+      securityGroups: [this.auroraSecurityGroup],
+      credentials: rds.Credentials.fromGeneratedSecret('admin'),
+      storageEncrypted: true,
+      deletionProtection: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
   }
 }
